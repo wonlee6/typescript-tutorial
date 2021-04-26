677e6a0b4618ea799612414151ffff1891322055
wonlee6 <wonlee6@gmail.com>
2021-04-26 16:27:33 +0900

    ts class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
 // node.js 는 typescript를 이해하지 못하기 때문에 javascript코드로 컴파일해야 한다
 
 // interface를 사용함으로써 객체를 넘김
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+// const person = {
+//   name: 'sangwon',
+//   age: 30,
+//   gender: 'female',
+// };
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person = {
-  name: 'sangwon',
-  age: 30,
-  gender: 'female',
-};
+
+const lynn = new Human('sangwon', 30, 'female');
 
 // ?를 붙임으로써 선택적 아규먼트가 됨
 // type을 정의할 수 도 있다.
@@ -22,6 +35,6 @@ const Sayhi = (person: Human): string => {
 // Sayhi(name, age, gender);
 // Sayhi(name, age); 오류 = 3개의 아규먼트로 설정했으니  2개는 오류로 뜸
 //                        typescript는 좀 더 규칙에 엄격함
-console.log(Sayhi(person));
+console.log(Sayhi(lynn));
 
 export {};
