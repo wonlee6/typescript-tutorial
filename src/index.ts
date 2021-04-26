f85bc5c3124d6a3d3f6ba2f4a96a355f958bd49e
wonlee6 <wonlee6@gmail.com>
2021-04-26 16:42:49 +0900

    blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,26 @@
-// node.js 는 typescript를 이해하지 못하기 때문에 javascript코드로 컴파일해야 한다
-
-// interface를 사용함으로써 객체를 넘김
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-// const person = {
-//   name: 'sangwon',
-//   age: 30,
-//   gender: 'female',
-// };
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public tiemstamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.tiemstamp = timestamp;
   }
 }
+const genesisBlock: Block = new Block(0, '23232323', '', 'Hello', 123456);
+// blockchain = block 연결
+let blockchain: [Block] = [genesisBlock];
 
-const lynn = new Human('sangwon', 30, 'female');
-
-// ?를 붙임으로써 선택적 아규먼트가 됨
-// type을 정의할 수 도 있다.
-// function을 string으로 반환하도록 설정할 수 있다.
-const Sayhi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are ${person.gender}`;
-};
-
-// Sayhi(name, age, gender);
-// Sayhi(name, age); 오류 = 3개의 아규먼트로 설정했으니  2개는 오류로 뜸
-//                        typescript는 좀 더 규칙에 엄격함
-console.log(Sayhi(lynn));
-
+console.log(blockchain);
 export {};
